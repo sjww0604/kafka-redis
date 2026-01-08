a6937b99e94bb83743167e63f7db8509a12eacb2
sjww0306 <sjww03@gmail.com>
2026-01-08 10:19:35 +0900

    Initial commit

---
+++ src/main/java/org/example/kafkaredis/KafkaRedisApplication.java
@@ -0,0 +1,13 @@
+package org.example.kafkaredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRedisApplication.class, args);
+	}
+
+}
